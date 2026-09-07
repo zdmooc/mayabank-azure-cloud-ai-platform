0e10eb7dae6781e79cb164e99728cc4ace2c1b50
zidane.djamal@gmail.com <zidane.djamal@gmail.com>
2026-09-07 11:59:46 +0200

    feat(iteration-1): add lab Terraform provider

---
+++ labs/lab-01-landing-zone/terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.6, < 2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_subscription" "current" {}
