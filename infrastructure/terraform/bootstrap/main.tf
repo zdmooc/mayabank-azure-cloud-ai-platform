1db61523c4b888329296c22292bd0bad5fb06894
zidane.djamal@gmail.com <zidane.djamal@gmail.com>
2026-09-13 11:32:08 +0200

    feat(iac): implement Azure Storage remote-state bootstrap

---
+++ infrastructure/terraform/bootstrap/main.tf
@@ -0,0 +1,41 @@
+resource "azurerm_resource_group" "tfstate" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  min_tls_version                 = "TLS1_2"
+  https_traffic_only_enabled      = true
+  public_network_access_enabled   = true
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false
+  infrastructure_encryption_enabled = true
+
+  blob_properties {
+    versioning_enabled = true
+
+    delete_retention_policy {
+      days = 14
+    }
+
+    container_delete_retention_policy {
+      days = 14
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = var.container_name
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
